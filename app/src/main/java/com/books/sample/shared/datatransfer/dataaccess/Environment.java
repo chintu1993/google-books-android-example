ec5dffe2c50396eb9f1b26b0b3703ad0b8d569a2
Ivan Martos <martos.ivan@gmail.com>
2017-06-04 22:25:33 +0200

    set api key and url

--- app/src/main/java/com/books/sample/shared/datatransfer/dataaccess/Environment.java
+++ app/src/main/java/com/books/sample/shared/datatransfer/dataaccess/Environment.java
@@ -2,7 +2,9 @@ package com.books.sample.shared.datatransfer.dataaccess;
 
 public class Environment {
 
-    public static final String BASE_URL = "";
+    public static final String BASE_URL = "https://www.googleapis.com/books/v1/";
+
+    public static final String API_KEY = "AIzaSyArn8v5mBZ2extEujnkcJ_k-MLlWgNM-E0";
     /**
      * Network connection timeout.
      */
