f93e27405147441fcd14e88f81f5ff8bd5b0cab5
Ivan Martos <martos.ivan@gmail.com>
2017-06-04 22:25:58 +0200

    initializing timber, joda and stetho

--- app/src/main/java/com/books/sample/BooksApplication.java
+++ app/src/main/java/com/books/sample/BooksApplication.java
@@ -3,20 +3,32 @@ package com.books.sample;
 import android.app.Activity;
 import android.app.Application;
 
-import com.books.sample.shared.dependency.injection.DaggerIApplicationComponent;
+import com.books.sample.shared.dependency.injection.Injector;
+import com.books.sample.shared.utils.infrastructure.TagPrefixDebugTree;
+import com.facebook.stetho.Stetho;
+
+import net.danlew.android.joda.JodaTimeAndroid;
 
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasDispatchingActivityInjector;
+import timber.log.Timber;
 
 import javax.inject.Inject;
 
 public class BooksApplication extends Application implements HasDispatchingActivityInjector {
+    public static final String LOG_TAG_PREFIX = "BOOKS:";
+
     @Inject DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        DaggerIApplicationComponent.create().inject(this);
+
+        JodaTimeAndroid.init(this);
+        Timber.plant(new TagPrefixDebugTree(LOG_TAG_PREFIX));
+        Stetho.initializeWithDefaults(this);
+        Injector.INSTANCE.initialize();
+        Injector.INSTANCE.getApplicationComponent().inject(this);
     }
 
     @Override
