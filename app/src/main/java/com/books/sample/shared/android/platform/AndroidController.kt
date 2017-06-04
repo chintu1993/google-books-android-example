4bcbbf64f98b1a65e9bc63f80398cea94e0e6239
Ivan Martos <martos.ivan@gmail.com>
2017-06-04 22:24:44 +0200

    added method for retrieving strings

--- app/src/main/java/com/books/sample/shared/android/platform/AndroidController.kt
+++ app/src/main/java/com/books/sample/shared/android/platform/AndroidController.kt
@@ -1,10 +1,15 @@
 package com.books.sample.shared.android.platform
 
 import android.content.Context
+import android.support.annotation.StringRes
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class AndroidController @Inject constructor() {
     lateinit var context: Context
+
+    fun getString(@StringRes stringId: Int): String {
+        return context.getString(stringId)
+    }
 }
