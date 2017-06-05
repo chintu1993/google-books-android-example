cdd991adf40feceadb179ea73cd49b8dade79389
Ivan Martos <martos.ivan@gmail.com>
2017-06-05 22:29:47 +0200

    adapter and fragment for list

---
+++ app/src/main/java/com/books/sample/booklist/ui/BookListAdapter.java
@@ -0,0 +1,88 @@
+package com.books.sample.booklist.ui;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.books.sample.R;
+import com.books.sample.booklist.domain.Book;
+import com.bumptech.glide.Glide;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+import java.util.List;
+
+public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.BookViewHolder> {
+
+    private final Context context;
+    private final OnBookClickListener onBookClickListener;
+    private List<Book> bookList = Lists.newArrayList();
+
+    public BookListAdapter(Context context, OnBookClickListener onBookClickListener) {
+        this.context = context;
+        this.onBookClickListener = onBookClickListener;
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.book_list__item_layout, parent, false);
+        return new BookViewHolder(itemView);
+    }
+
+    public void addItems(List<Book> addedBookList) {
+        int previousItemPosition = bookList.size() > 0 ? bookList.size() - 1 : 0;
+        bookList.addAll(addedBookList);
+        notifyItemRangeInserted(previousItemPosition, addedBookList.size());
+    }
+
+    public void clearItems() {
+        int previousCount = bookList.size();
+        bookList = Lists.newArrayList();
+        notifyItemRangeRemoved(0, previousCount);
+    }
+
+    @Override
+    public void onBindViewHolder(BookViewHolder holder, int position) {
+        Timber.d("onBindViewHolder %s", position);
+        final Book book = bookList.get(position);
+        holder.title.setText(book.getTitle());
+        holder.author.setText(Joiner.on(", ").join(book.getAuthors()));
+        Glide.with(context).load(book.getThumbnail().getValue()).into(holder.thubmnail);
+        holder.itemView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBookClickListener.onBookClicked(book);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    public interface OnBookClickListener {
+        void onBookClicked(Book book);
+    }
+
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.thumbnail_image_view) public ImageView thubmnail;
+        @BindView(R.id.title_text_view) public TextView title;
+        @BindView(R.id.author_text_view) public TextView author;
+
+        BookViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/com/books/sample/booklist/ui/BookListFragment.java
+++ app/src/main/java/com/books/sample/booklist/ui/BookListFragment.java
@@ -1,11 +1,41 @@
 package com.books.sample.booklist.ui;
 
-import android.app.Fragment;
+import static com.books.sample.shared.utils.infrastructure.UiUtils.dpToPx;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
+import android.view.View;
 
 import com.books.sample.R;
+import com.books.sample.booklist.domain.Book;
+import com.books.sample.booklist.platform.BookListController;
+import com.books.sample.booklist.platform.BookListListener;
+import com.books.sample.booklist.ui.BookListAdapter.OnBookClickListener;
 import com.books.sample.shared.ui.BaseFragment;
+import com.books.sample.shared.ui.GridSpacingItemDecoration;
 
-public class BookListFragment extends BaseFragment {
+import org.jetbrains.annotations.NotNull;
+
+import butterknife.BindView;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class BookListFragment extends BaseFragment implements OnBookClickListener, BookListListener {
+
+    @Inject BookListController bookListController;
+
+    @BindView(R.id.book_list_recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.search) SearchView searchView;
+
+    private BookListAdapter bookListAdapter;
 
     @Override
     protected int getLayoutResourceId() {
@@ -15,5 +45,58 @@ public class BookListFragment extends BaseFragment {
     @Override
     protected Fragment getThis() {
         return this;
+    }
+
+    @Override
+    protected void ofAfterViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        bookListAdapter = new BookListAdapter(getContext(), this);
+        bookListController.setBookListListener(this);
+
+        searchView.setQueryHint("Find books");
+        searchView.onActionViewExpanded();
+        searchView.setIconified(false);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                bookListController.requestBooks(query);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.setQuery("flower", true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10, getResources()), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(bookListAdapter);
+
+    }
+
+    @Override
+    public void onBookClicked(Book book) {
+
+    }
+
+
+    @Override
+    public void onBooksRequested() {
+
+    }
+
+    @Override
+    public void onBooksRetrieved(@NotNull List<Book> bookList) {
+        bookListAdapter.addItems(bookList);
+    }
+
+    @Override
+    public void onClearBooks() {
+        bookListAdapter.clearItems();
     }
 }
