14870041022debb76fb22c5d60d7c3501b26b2fb
Ivan Martos <martos.ivan@gmail.com>
2017-06-07 10:22:20 +0200

    removed options menu from activity

--- app/src/main/java/com/books/sample/MainActivity.java
+++ app/src/main/java/com/books/sample/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.books.sample.booklist.ui.BookListFragment;
 import com.books.sample.shared.android.platform.AndroidController;
@@ -34,28 +32,6 @@ public class MainActivity extends AppCompatActivity implements HasDispatchingSup
         setSupportActionBar(toolbar);
 
         screenFlowController.showInitialFragment(new BookListFragment());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
