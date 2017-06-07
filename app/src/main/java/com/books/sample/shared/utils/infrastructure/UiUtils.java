0e4142d19e7fca404d974179b1c3ac48e45ef2d3
Ivan Martos <martos.ivan@gmail.com>
2017-06-07 08:03:09 +0200

    added more view util methods

--- app/src/main/java/com/books/sample/shared/utils/infrastructure/UiUtils.java
+++ app/src/main/java/com/books/sample/shared/utils/infrastructure/UiUtils.java
@@ -1,7 +1,17 @@
 package com.books.sample.shared.utils.infrastructure;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Arrays.asList;
+
 import android.content.res.Resources;
 import android.util.TypedValue;
+import android.view.View;
+
+import java.util.List;
 
 public class UiUtils {
     /**
@@ -9,5 +19,24 @@ public class UiUtils {
      */
     public static int dpToPx(int dp, Resources r) {
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+    public static void setVisible(boolean visible, View firstView, View... otherViews) {
+        int visibility = visible ? VISIBLE : GONE;
+
+        for (View view : mergeAndCheckVarargs(firstView, otherViews)) {
+            view.setVisibility(visibility);
+        }
+    }
+
+    private static <T> List<T> mergeAndCheckVarargs(T firstItem, T[] otherItems) {
+        checkNotNull(firstItem);
+        checkNotNull(otherItems);
+        checkArgument(!asList(otherItems).contains(null));
+
+        List<T> result = newArrayList(otherItems);
+        result.add(0, firstItem);
+
+        return result;
     }
 }
