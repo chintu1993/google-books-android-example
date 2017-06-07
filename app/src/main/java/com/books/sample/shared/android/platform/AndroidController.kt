dd7b24af2fb800176d6d6108e3f14b4821bb38fa
Ivan Martos <martos.ivan@gmail.com>
2017-06-07 08:47:28 +0200

    method for starting intent

--- app/src/main/java/com/books/sample/shared/android/platform/AndroidController.kt
+++ app/src/main/java/com/books/sample/shared/android/platform/AndroidController.kt
@@ -1,6 +1,7 @@
 package com.books.sample.shared.android.platform
 
 import android.content.Context
+import android.content.Intent
 import android.support.annotation.StringRes
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,5 +12,10 @@ class AndroidController @Inject constructor() {
 
     fun getString(@StringRes stringId: Int): String {
         return context.getString(stringId)
+    }
+
+    fun openWebIntent(uri: com.books.sample.shared.domain.Uri) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, uri.value)
+        context.startActivity(browserIntent)
     }
 }
